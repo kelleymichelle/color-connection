9bfa4f96a3b267137f03d05fb6eae3532b9e0f81
Kelley Chaplain <kmchaplain@gmail.com>
2020-02-21 21:47:48 -0600

    added redux and created "store". added navbar and links for signup and login. Added bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar>
+      <Navbar.Brand href="/">Color Connection</Navbar.Brand>
+          <Nav className="ml auto">
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Log In</Link>
+            <Link to="/about">About Us</Link>
+          </Nav>
+      </Navbar>
+    </Router>
   );
 }
 
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,8 @@
+const userReducer = (state = {
+  currentUser: {},
+}, action) => {
+
+  return state
+}
+
+export default userReducer
