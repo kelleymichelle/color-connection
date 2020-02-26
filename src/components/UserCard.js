21e8dabf648c48bc24a9489b5d9e60174db9d6db
Kelley Chaplain <kmchaplain@gmail.com>
2020-02-26 17:21:24 -0600

    added Link to on user card and passing props through

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,16 +1,26 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 import Card from 'react-bootstrap/Card'
 
 export default function UserCard(props) {
   const user = props.user
+  const userPath = `/user/${user.id}`
   return (
-    <Card style={{ width: '19em' }}>
-      <Card.Body>
-        <Card.Img style={{ width: '80%' }} variant="top" src={user.image} />
-        <Card.Title>{user.name}</Card.Title>
-        <Card.Subtitle>{user.location}</Card.Subtitle>
-      </Card.Body>
-    </Card>
+    <Link to={{
+      pathname: userPath,
+      state: {
+        user: user
+      }
+    }}>
+      <Card style={{ width: '19em' }}>
+        <Card.Body>
+          <Card.Img style={{ width: '80%' }} variant="top" src={user.image} />
+          <Card.Title>{user.name}</Card.Title>
+          <Card.Subtitle>{user.location}</Card.Subtitle>
+        </Card.Body>
+      </Card>
+    </Link>
   )
 }
