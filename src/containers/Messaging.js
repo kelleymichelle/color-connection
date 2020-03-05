9cfdef0926eb1d9afa053a5eafc1bfff6eb0362a
Kelley Chaplain <kmchaplain@gmail.com>
2020-03-04 20:52:57 -0600

    created messaging container. Passing currentUser through App and getting other user through props params.

--- src/containers/Messaging.js
+++ src/containers/Messaging.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import axios from 'axios'
 
 export default class Messaging extends React.Component {
   state = {
     currentUser: '',
     friend: ''
+  }
+
+  componentDidMount() {
+    const userId = this.props.match.params.userId
+    this.fetchUser(userId)
   }
 
   componentDidUpdate(prevProps) {
@@ -12,6 +18,18 @@ export default class Messaging extends React.Component {
         currentUser: this.props.currentUser
       })
     }
+  }
+
+  fetchUser = id => {
+    axios.get(`http://localhost:3001/users/${id}`,
+    {withCredentials: true})
+    .then(response => {
+      console.log(response.data.user)
+      this.setState({
+        friend: response.data.user
+      })
+    })
+    .catch(error => console.log(error))
   }
 
   render() {
