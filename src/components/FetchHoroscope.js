96fdf60eadc993fbe1d87ea2007226fed790e566
Kelley Chaplain <kmchaplain@gmail.com>
2020-03-10 09:48:23 -0500

    added conditional to horoscope fetch to prevent undefined signs from attempting to fetch

--- src/components/FetchHoroscope.js
+++ src/components/FetchHoroscope.js
@@ -29,26 +29,27 @@ export default class FetchHoroscope extends React.Component {
   }
 
   fetchData = (sign) => {
-    const horoscopeURL = `https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${sign}&day=today`
-      fetch(horoscopeURL, {
-        "method": "POST",
-        "headers": {
-          "x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com",
-          "x-rapidapi-key": "c3ef217e69msh026e456e9121fbdp1cde79jsn3daf9d88f670",
-          "content-type": "application/x-www-form-urlencoded"
-        },
-        "body": {}
-      })
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          data: data
+    if (sign !== undefined) {
+      const horoscopeURL = `https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${sign}&day=today`
+        fetch(horoscopeURL, {
+          "method": "POST",
+          "headers": {
+            "x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com",
+            "x-rapidapi-key": "c3ef217e69msh026e456e9121fbdp1cde79jsn3daf9d88f670",
+            "content-type": "application/x-www-form-urlencoded"
+          },
+          "body": {}
         })
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            data: data
+          })
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
 
   render() {
---
+++ src/components/dashboard/SuggestedConnections.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class SuggestedConnections extends React.Component {
+  
+  
+  render() {
+    return (
+      <div>
+        Suggested Connections
+      </div>
+    )
+  }
+}
