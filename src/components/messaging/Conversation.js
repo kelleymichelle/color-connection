01e59bd9d809c12de109a78afdd08c626c505451
Kelley Chaplain <kmchaplain@gmail.com>
2020-03-09 22:10:46 -0500

    created conversation view in inbox for conversation currentUseris involved in. Link on each conversation to open thread

--- src/components/messaging/Conversation.js
+++ src/components/messaging/Conversation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import axios from 'axios'
+import Card from 'react-bootstrap/Card'
 
 export default class Conversation extends React.Component {
 
@@ -17,9 +18,9 @@ export default class Conversation extends React.Component {
 
   fetchUserData = () => {
     const currentUser = this.props.currentUser
-    const convo = this.props.convo[0]
+    const convo = this.props.convo
     // debugger
-    const userId = currentUser.id === convo.sender_id ? convo.reciever_id : convo.sender_id
+    const userId = currentUser.id === convo[0].sender_id ? convo[0].reciever_id : convo[0].sender_id
     console.log(userId)
 
     axios.get(`http://localhost:3001/users/${userId}`,
@@ -34,8 +35,17 @@ export default class Conversation extends React.Component {
   }
 
   render() {
+    const userLink = `/messaging/${this.state.friend.id}`
     return (
-      null
+      <Card style={{margin: '15px', padding: '15px'}}>
+        <div className="d-flex">
+          <div style={{width: '70px', marginRight: '15px'}}><img style={{width: '100%'}} src={this.state.friend.image} alt={this.state.friend.name} /></div>
+          <div>
+          <Card.Title>{this.state.friend.name}</Card.Title>
+          <Card.Link href={userLink}>view conversation</Card.Link>
+          </div>
+        </div>
+      </Card>
     )
   }
 }
