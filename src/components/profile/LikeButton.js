eb46952d8e1ffed96dfe786e9bff768ad1c103bd
Kelley Chaplain <kmchaplain@gmail.com>
2020-03-20 15:51:21 -0500

    changed heart icons for likes to bootstrap buttons that display relevant text

--- src/components/profile/LikeButton.js
+++ src/components/profile/LikeButton.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Button from 'react-bootstrap/Button'
+
 // import { connect } from 'react-redux'
 
 
@@ -24,19 +26,21 @@ export default class LikeButton extends React.Component {
     const following = this.state.following
     // debugger
     // console.log(user)
-    const unheart = 'https://res.cloudinary.com/color-connection/image/upload/v1583002402/unheart_nnzhw0.png'
-    const heart = 'https://res.cloudinary.com/color-connection/image/upload/v1583002402/heart_b794ji.png'
+    // const unheart = 'https://res.cloudinary.com/color-connection/image/upload/v1583002402/unheart_nnzhw0.png'
+    // const heart = 'https://res.cloudinary.com/color-connection/image/upload/v1583002402/heart_b794ji.png'
     // const currentUser = this.props.currentUser
 
     if ( following && following.find(u => u.id === user.id) ) {
       return (
-        <img onClick={this.props.handleUnlikeClick} style={{ width: '5%', marginTop: '10px'}} src={heart} alt="heart" />
+        // <img onClick={this.props.handleUnlikeClick} style={{ width: '5%', marginTop: '10px'}} src={heart} alt="heart" />
+        <Button onClick={this.props.handleUnlikeClick} size="lg">Unlike</Button>
       )
     } else if ( this.props.currentUser.id === user.id ) {
       return null
     } else {
       return (
-          <img onClick={this.props.handleLikeClick} style={{ width: '5%', marginTop: '10px'}} src={unheart} alt="unheart" />
+          // <img onClick={this.props.handleLikeClick} style={{ width: '5%', marginTop: '10px'}} src={unheart} alt="unheart" />
+          <Button onClick={this.props.handleLikeClick} size="lg">Like</Button>
       )
     }
   }
